35348c01d869224bb68f4db776936ab2e49f9eac
mhsmith321 <marty.smith01@yahoo.com>
2022-04-17 14:03:03 -0400

    render numberSlice.js data in App.jsx file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import { useSelector } from 'react-redux';
 
 function App() {
+  const number = useSelector(state => state.number.value);
   return (
     <div className="App">
       <h1>Hello, world!</h1>
+      <h2>Change the Number</h2>
+      <button>-</button>
+      {number}
+      <button>+</button>
     </div>
   );
 }
