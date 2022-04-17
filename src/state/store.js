975f7aca2df751961f62f521789658c8a7cb821c
mhsmith321 <marty.smith01@yahoo.com>
2022-04-17 13:59:49 -0400

    configure store.js to use numberSlice.js data and reducer

--- src/state/store.js
+++ src/state/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
+import numberReducer from './numberSlice';
 
 export const store = configureStore({
-    reducer: {}
+    reducer: {
+        number: numberReducer
+    }
 });
